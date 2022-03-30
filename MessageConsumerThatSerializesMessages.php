EventSaucePHP/EventSauce

Prefer Message::payload over Message::event

src/TestUtilities/MessageConsumerThatSerializesMessages.php
@@ -26,6 +26,6 @@ class MessageConsumerThatSerializesMessages implements MessageConsumer
     {
         $payload = $this->serializer->serializeMessage($message);
         $deserializedMessage = $this->serializer->unserializePayload($payload);
-        TestCase::assertEquals($message->event(), $deserializedMessage->event());
+        TestCase::assertEquals($message->payload(), $deserializedMessage->payload());
     }
 }

EventSaucePHP/TestUtilities

MessageConsumerThatSerializesMessages.php
@@ -33,6 +33,6 @@ class MessageConsumerThatSerializesMessages implements MessageConsumer
         $deserializedMessage = $this->serializer->unserializePayload(
             json_decode($payloadAsString, true)
         );
-        TestCase::assertEquals($message->event(), $deserializedMessage->event());
+        TestCase::assertEquals($message->payload(), $deserializedMessage->event());
     }
 }